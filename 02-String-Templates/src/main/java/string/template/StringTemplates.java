ead60a3d0b508f3e2730dfd4ea7f958101b95477
Falk Sippach <falk@jug-da.de>
2023-10-18 11:54:33 +0200

    Übung 3

--- 02-String-Templates/src/main/java/string/template/StringTemplates.java
+++ 02-String-Templates/src/main/java/string/template/StringTemplates.java
@@ -3,6 +3,8 @@ package string.template;
 
 import java.sql.*;
 
+import static java.util.FormatProcessor.FMT;
+
 /**
  * The template expression STR."My name is \{name}" consists of:
  *
@@ -23,5 +25,12 @@ public class StringTemplates {
         String info = STR."User '\{user.email()}' hat die Rolle '\{user.role().name()}'";
         System.out.println(info); // User 'dieter@develop.de' hat die Rolle 'admin'
 
+        String s = FMT. "Zahl: %10.2f\{ 3.00911 }" ;
+        System.out.println(s);
+
+        var s2 = STR."""
+            Zeile 1: \{ "abc" }
+            Zeile 2: \{s}""";
+        System.out.println(s2);
     }
 }
